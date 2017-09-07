f83efca2b604b21b4b0ee85926b9d81fefd84319
vrieraba <31625604+vrieraba@users.noreply.github.com>
2017-09-07 17:28:15 +0200

    Initial commit

---
+++ backend/src/main/java/com/vrieraba/apps/easycomm/EasycommApplication.java
@@ -0,0 +1,12 @@
+package com.vrieraba.apps.easycomm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasycommApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasycommApplication.class, args);
+	}
+}
---
+++ backend/src/test/java/com/vrieraba/apps/easycomm/EasycommApplicationTests.java
@@ -0,0 +1,16 @@
+package com.vrieraba.apps.easycomm;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EasycommApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
